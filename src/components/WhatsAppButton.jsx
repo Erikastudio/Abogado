684e792b4aa1930515763a92ce398af1cc3b8bb4
Erikastudio <julieth.torres8@gmail.com>
2026-06-25 11:57:14 -0500

    whatsapp actualizado

--- src/components/WhatsAppButton.jsx
+++ src/components/WhatsAppButton.jsx
@@ -1,9 +1,11 @@
 export default function WhatsAppButton() {
-  const numero = "573001234567"; // cámbialo por el real
+  const numero = "573134623765";
+
+  const mensaje = "Hola, estoy interesado en una asesoría jurídica.";
 
   return (
     <a
-      href={`https://wa.me/${numero}?text=Hola,%20necesito%20asesoría%20jurídica`}
+      href={`https://wa.me/${numero}?text=${encodeURIComponent(mensaje)}`}
       target="_blank"
       rel="noreferrer"
       className="fixed bottom-6 right-6 bg-green-500 text-white px-6 py-4 rounded-full shadow-xl font-bold hover:scale-105 transition"
